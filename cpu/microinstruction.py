cdceaba85a044a9ca6cf7e89e008b841ef80cb5b
John Whipple <john@whipple.org>
2017-04-16 04:10:21 -0400

    Added cycles and operations to most of the implied mode instructions

--- cpu/microinstruction.py
+++
@@ -1,3 +0,0 @@
-class Microinstruction:
-    def __init__(self):
-        pass
--- cpu/operation.py
+++ cpu/operation.py
@@ -1,4 +1,53 @@
+from enum import Enum, auto
+
+
+class OperationType(Enum):
+    set = auto()
+    clear = auto()
+    move = auto()
+    alu = auto()
+
+
+class AluFunction(Enum):
+    increment = auto()
+    decrement = auto()
+    add = auto()
+    subtract = auto()
+    multiply = auto()
+    divide = auto()
+    compare = auto()
+    and_ = auto()
+    or_ = auto()
+    xor = auto()
+    shift_left = auto()
+    shift_right = auto()
+    rotate_left = auto()
+    rotate_right = auto()
+
+
 class Operation:
-    def __init__(self, dst, src):
-        self.dst = dst
+    pass
+
+
+class SetFlagOperation(Operation):
+    def __init__(self, flag):
+        self.flag = flag
+
+
+class ClearFlagOperation(Operation):
+    def __init__(self, flag):
+        self.flag = flag
+
+
+class MoveOperation(Operation):
+    def __init__(self, src, dst):
         self.src = src
+        self.dst = dst
+
+
+class AluOperation(Operation):
+    def __init__(self, input_a, input_b, alu_functionn, dst_register):
+        self.input_a = input_a
+        self.input_b = input_b
+        self.alu_function = alu_functionn
+        self.dst_register = dst_register
