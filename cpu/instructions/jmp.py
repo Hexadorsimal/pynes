52d8dbf9b363f779305e35c4511ed10b0df8deae
John Whipple <john@whipple.org>
2017-10-16 18:25:10 -0400

    Converted JMP to Python

---
+++ cpu/instructions/jmp.py
@@ -0,0 +1,42 @@
+from ..cycle import Cycle
+from ..operation import MoveOperation
+from ..absolute_instruction import AbsoluteInstruction
+from ..indirect_absolute_instruction import IndirectAbsoluteInstruction
+
+
+class JmpAbsolute(AbsoluteInstruction):
+    def __init__(self):
+        super().__init__()
+        self.cycles.append(Cycle([MoveOperation('ADL', 'PCL')]))
+        self.cycles.append(Cycle([MoveOperation('ADH', 'PCH')]))
+
+    @property
+    def name(self):
+        return 'JMP'
+
+    @property
+    def opcode(self):
+        return 0x4C
+
+    @property
+    def description(self):
+        return 'Jump to New Location'
+
+
+class JmpIndirectAbsolute(IndirectAbsoluteInstruction):
+    def __init__(self):
+        super().__init__()
+        self.cycles.append(Cycle([MoveOperation('ADL', 'PCL')]))
+        self.cycles.append(Cycle([MoveOperation('ADH', 'PCH')]))
+
+    @property
+    def name(self):
+        return 'JMP'
+
+    @property
+    def opcode(self):
+        return 0x6C
+
+    @property
+    def description(self):
+        return 'Jump to New Location'
