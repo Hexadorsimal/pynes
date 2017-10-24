abbacf3506767327ddf4c08717ee0a9835400adf
John Whipple <john@whipple.org>
2017-10-23 20:56:38 -0400

    Converted ORA to python

--- nes/cpu/instructions/ora.py
+++ nes/cpu/instructions/ora.py
@@ -1,5 +1,5 @@
 from nes.cpu.cycle import Cycle
-from nes.cpu.microinstructions import BitwiseAndMicroinstruction
+from nes.cpu.microinstructions import BitwiseOrMicroinstruction
 from .immediate_instruction import ImmediateInstruction
 from .zeropage_instruction import ZeroPageInstruction
 from .zeropagex_instruction import ZeroPageXInstruction
@@ -10,145 +10,145 @@ from .indexed_indirect_instruction import IndexedIndirectInstruction
 from .indirect_indexed_instruction import IndirectIndexedInstruction
 
 
-class AndImmediate(ImmediateInstruction):
+class OraImmediate(ImmediateInstruction):
     def __init__(self):
         super().__init__()
-        self.cycles.append(Cycle([BitwiseAndMicroinstruction('A', 'A', 'DL')]))
+        self.cycles.append(Cycle([BitwiseOrMicroinstruction('A', 'A', 'DL')]))
 
     @property
     def name(self):
-        return 'AND'
+        return 'ORA'
 
     @property
     def opcode(self):
-        return 0x29
+        return 0x09
 
     @property
     def description(self):
-        return 'AND Memory with Accumulator'
+        return 'OR Memory with Accumulator'
 
 
-class AndZeroPage(ZeroPageInstruction):
+class OraZeroPage(ZeroPageInstruction):
     def __init__(self):
         super().__init__()
-        self.cycles.append(Cycle([BitwiseAndMicroinstruction('A', 'A', 'DL')]))
+        self.cycles.append(Cycle([BitwiseOrMicroinstruction('A', 'A', 'DL')]))
 
     @property
     def name(self):
-        return 'AND'
+        return 'ORA'
 
     @property
     def opcode(self):
-        return 0x25
+        return 0x05
 
     @property
     def description(self):
-        return 'AND Memory with Accumulator'
+        return 'OR Memory with Accumulator'
 
 
-class AndZeroPageX(ZeroPageXInstruction):
+class OraZeroPageX(ZeroPageXInstruction):
     def __init__(self):
         super().__init__()
-        self.cycles.append(Cycle([BitwiseAndMicroinstruction('A', 'A', 'DL')]))
+        self.cycles.append(Cycle([BitwiseOrMicroinstruction('A', 'A', 'DL')]))
 
     @property
     def name(self):
-        return 'AND'
+        return 'ORA'
 
     @property
     def opcode(self):
-        return 0x35
+        return 0x15
 
     @property
     def description(self):
-        return 'AND Memory with Accumulator'
+        return 'OR Memory with Accumulator'
 
 
-class AndAbsolute(AbsoluteInstruction):
+class OraAbsolute(AbsoluteInstruction):
     def __init__(self):
         super().__init__()
-        self.cycles.append(Cycle([BitwiseAndMicroinstruction('A', 'A', 'DL')]))
+        self.cycles.append(Cycle([BitwiseOrMicroinstruction('A', 'A', 'DL')]))
 
     @property
     def name(self):
-        return 'AND'
+        return 'ORA'
 
     @property
     def opcode(self):
-        return 0x2D
+        return 0x0D
 
     @property
     def description(self):
-        return 'AND Memory with Accumulator'
+        return 'OR Memory with Accumulator'
 
 
-class AndAbsoluteX(AbsoluteXInstruction):
+class OraAbsoluteX(AbsoluteXInstruction):
     def __init__(self):
         super().__init__()
-        self.cycles.append(Cycle([BitwiseAndMicroinstruction('A', 'A', 'DL')]))
+        self.cycles.append(Cycle([BitwiseOrMicroinstruction('A', 'A', 'DL')]))
 
     @property
     def name(self):
-        return 'AND'
+        return 'ORA'
 
     @property
     def opcode(self):
-        return 0x3D
+        return 0x1D
 
     @property
     def description(self):
-        return 'AND Memory with Accumulator'
+        return 'OR Memory with Accumulator'
 
 
-class AndAbsoluteY(AbsoluteYInstruction):
+class OraAbsoluteY(AbsoluteYInstruction):
     def __init__(self):
         super().__init__()
-        self.cycles.append(Cycle([BitwiseAndMicroinstruction('A', 'A', 'DL')]))
+        self.cycles.append(Cycle([BitwiseOrMicroinstruction('A', 'A', 'DL')]))
 
     @property
     def name(self):
-        return 'AND'
+        return 'ORA'
 
     @property
     def opcode(self):
-        return 0x39
+        return 0x19
 
     @property
     def description(self):
-        return 'AND Memory with Accumulator'
+        return 'OR Memory with Accumulator'
 
 
-class AndIndexedIndirect(IndexedIndirectInstruction):
+class OraIndexedIndirect(IndexedIndirectInstruction):
     def __init__(self):
         super().__init__()
-        self.cycles.append(Cycle([BitwiseAndMicroinstruction('A', 'A', 'DL')]))
+        self.cycles.append(Cycle([BitwiseOrMicroinstruction('A', 'A', 'DL')]))
 
     @property
     def name(self):
-        return 'AND'
+        return 'ORA'
 
     @property
     def opcode(self):
-        return 0x21
+        return 0x01
 
     @property
     def description(self):
-        return 'AND Memory with Accumulator'
+        return 'OR Memory with Accumulator'
 
 
-class AndIndirectIndexed(IndirectIndexedInstruction):
+class OraIndirectIndexed(IndirectIndexedInstruction):
     def __init__(self):
         super().__init__()
-        self.cycles.append(Cycle([BitwiseAndMicroinstruction('A', 'A', 'DL')]))
+        self.cycles.append(Cycle([BitwiseOrMicroinstruction('A', 'A', 'DL')]))
 
     @property
     def name(self):
-        return 'AND'
+        return 'ORA'
 
     @property
     def opcode(self):
-        return 0x31
+        return 0x11
 
     @property
     def description(self):
-        return 'AND Memory with Accumulator'
+        return 'OR Memory with Accumulator'
