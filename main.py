c90da9d57e3e367663be6c2eb1bceb5be30d2799
John Whipple <john@whipple.org>
2019-08-09 20:57:56 -0400

    moved the pygame rendering into the ppu broke out the palette drawing into functions

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pygame
 import yaml
 from nes import Nes, CartridgeFactory
 
@@ -15,34 +14,5 @@ if __name__ == '__main__':
     for i in range(32):
         nes.ppu.bus.write(0x3F00 + i, i)
 
-    pygame.init()
-    pygame.display.set_caption('NES Palette')
-    screen = pygame.display.set_mode((256, 240))
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        rect = pygame.Rect(0, 0, 16, 16)
-        for color in nes.ppu.system_palette.colors:
-            screen.fill(color, rect)
-            rect.x += rect.w
-            if rect.x >= rect.w * 16:
-                rect.x = 0
-                rect.y += rect.h
-
-        rect.x = 0
-        rect.y = 64
-
-        for i in range(32):
-            color_index = nes.ppu.bus.read(0x3F00 + i)
-            color = nes.ppu.system_palette.colors[color_index]
-            screen.fill(color, rect)
-            rect.x += rect.w
-            if rect.x >= rect.w * 16:
-                rect.x = 0
-                rect.y += rect.h
-
-        pygame.display.flip()
+    while True:
+        nes.ppu.tick()
