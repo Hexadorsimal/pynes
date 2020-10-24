d3222f84a3e0a5ae1be2199144c64c42eee06966
John Whipple <john@whipple.org>
2020-10-23 22:16:56 -0400

    Fixed this code so it's not a string of if statements.

--- nes/ppu/ppu_register_set.py
+++ nes/ppu/ppu_register_set.py
@@ -5,39 +5,19 @@ class PpuRegisterSet(BusDevice):
     def __init__(self, ppu):
         super().__init__(8)
         self.ppu = ppu
+        self.registers = [
+            ppu.ppu_controller,
+            ppu.ppu_mask,
+            ppu.ppu_status,
+            ppu.oam_address,
+            ppu.oam_data,
+            ppu.ppu_scroll,
+            ppu.address,
+            ppu.ppu_data,
+        ]
 
     def read(self, logical_addr):
-        if logical_addr == 0:
-            return self.ppu.ppu_controller
-        elif logical_addr == 1:
-            return self.ppu.ppu_mask
-        elif logical_addr == 2:
-            return self.ppu.ppu_status
-        elif logical_addr == 3:
-            return self.ppu.oam_address
-        elif logical_addr == 4:
-            return self.ppu.oam_data
-        elif logical_addr == 5:
-            return self.ppu.ppu_scroll
-        elif logical_addr == 6:
-            return self.ppu.ppu_address
-        elif logical_addr == 7:
-            return  self.ppu.ppu_data
+        return self.registers[logical_addr].read()
 
     def write(self, logical_addr, value):
-        if logical_addr == 0:
-            self.ppu.ppu_controller = value
-        elif logical_addr == 1:
-            self.ppu.ppu_mask = value
-        elif logical_addr == 2:
-            self.ppu.ppu_status = value
-        elif logical_addr == 3:
-            self.ppu.oam_address = value
-        elif logical_addr == 4:
-            self.ppu.oam_data = value
-        elif logical_addr == 5:
-            self.ppu.ppu_scroll = value
-        elif logical_addr == 6:
-            self.ppu.ppu_address = value
-        elif logical_addr == 7:
-            self.ppu.ppu_data = value
+        self.registers[logical_addr].write(value)
