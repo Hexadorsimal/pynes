7ad679288cc3340264665be3c342aed5b00ef20c
John Whipple <john.w.whipple@gmail.com>
2025-08-16 22:23:45 -0400

    Changed the dict of dicts to be a dict of objects

--- nes/bus/bus.py
+++ nes/bus/bus.py
@@ -1,41 +1,44 @@
 import logging
-from typing import Optional
+from dataclasses import dataclass
 
 from .address_range import AddressRange
 from .bus_device import BusDevice
 
 
+@dataclass
+class DeviceEntry:
+    device: BusDevice
+    addr_range: AddressRange
+
+
 class Bus(BusDevice):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.devices = {}
+        self.devices: dict[str, DeviceEntry] = {}
 
     def attach_device(self, name: str, device: BusDevice, addr: int, size: int) -> None:
-        self.devices[name] = {
-            'device': device,
-            'addr_range': AddressRange(addr, size),
-        }
+        self.devices[name] = DeviceEntry(device, AddressRange(addr, size))
 
     def detach_device(self, device_name: str):
         self.devices.pop(device_name)
 
-    def find_device_at_addr(self, addr: int) -> BusDevice | None:
+    def find_device_entry(self, addr: int) -> DeviceEntry | None:
         for entry in self.devices.values():
-            if addr in entry['addr_range']:
+            if addr in entry.addr_range:
                 return entry
 
         return None
 
     def read(self, addr: int) -> int:
-        entry = self.find_device_at_addr(addr)
+        entry = self.find_device_entry(addr)
         if entry:
-            return entry['device'].read(addr - entry['addr_range'].start)
+            return entry.device.read(addr - entry.addr_range.start)
         else:
             raise RuntimeError('Unhandled memory read request')
 
     def write(self, addr: int, value: int) -> None:
-        entry = self.find_device_at_addr(addr)
+        entry = self.find_device_entry(addr)
         if entry:
-            entry['device'].write(addr - entry['addr_range'].start, value)
+            entry.device.write(addr - entry.addr_range.start, value)
         else:
             raise RuntimeError('Unhandled memory write request')
